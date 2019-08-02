29f4c5d4e6ff5f0feb952658903c7703282d8c06
Sania Parekh <saniaparekh315@gmail.com>
2019-08-02 14:41:13 -0500

    finsihed string methods chapter

---
+++ Objects/string-methods.js
@@ -0,0 +1,27 @@
+let name = '  Patty Smith '
+
+//Length Property
+console.log(name.length);
+
+//Convert to Upper case
+console.log(name.toUpperCase());
+
+//Conver to lower case
+console.log(name.toLowerCase());
+
+//Includes Method
+let password = 'abc123'
+console.log(password.includes('fish'));
+
+//Trim 
+console.log(name.trim());
+
+let passValid = function(password) {
+        return password.length > 8 && !password.includes('password');
+        //this works the same way as saying it in an if statement and returning true if criteria is met. 
+    }
+   
+
+console.log(passValid('adso'));
+console.log(passValid('abeidkdkdkddkdkd'))
+console.log(passValid('abeidkdkdkddkdkdpassword'))
