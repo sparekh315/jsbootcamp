59ff73ae0dc6b60a3ab79500f9b86859ba180c21
Sania Parekh <saniaparekh315@gmail.com>
2019-08-06 14:17:26 -0500

    added income object to account array. Changed getAccountSummary to include income

--- Arrays/expense-tracker.js
+++ Arrays/expense-tracker.js
@@ -1,19 +1,34 @@
 const account = {
     name: 'Paula Black',
     expenses: [],
+    income: [],
     addExpense: function (description, amount) {
         this.expenses.push({
             description: description,
             amount: amount
         })
     },
+
+    addIncome: function (description, amount) {
+        this.income.push({
+            description: description,
+            amount: amount
+        })
+
+    },
+
     getAccountSummary: function () {
         let totalExpenses = 0
+        let totalIncome = 0
         this.expenses.forEach(function (expense){
             totalExpenses = totalExpenses + expense.amount
         })
+        this.income.forEach(function(addIncome){
+            totalIncome = totalIncome + addIncome.amount
+        })
 
-        return `${this.name} has $${totalExpenses.toFixed(2)} in expenses.`
+
+        return `${this.name} has a balance of $${totalIncome - totalExpenses}. $${totalIncome.toFixed(2)} in income, and $${totalExpenses.toFixed(2)} in expenses.`
     }
 }
 
@@ -25,7 +40,8 @@ const account = {
 
 
 
-
+account.addIncome('freelance', 4000)
 account.addExpense('coffee', 3.50)
 account.addExpense('plane ticket', 400)
+console.log(account)
 console.log(account.getAccountSummary())
