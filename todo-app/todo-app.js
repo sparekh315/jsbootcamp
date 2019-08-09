54b2fa129161fdd1da5b24497c7de09f8a0cfdf9
Sania Parekh <saniaparekh315@gmail.com>
2019-08-09 16:29:59 -0500

    DOM challenge

---
+++ todo-app/todo-app.js
@@ -0,0 +1,10 @@
+
+
+const paragraphs = document.querySelectorAll('p') 
+
+paragraphs.forEach(function (p) {
+    if (p.textContent.includes('the')) {
+        p.remove() //removes the values
+    }
+  
+})
