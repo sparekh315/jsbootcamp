d6a786db27f527f7fdc6830b88f84b432138b52e
Sania Parekh <saniaparekh315@gmail.com>
2019-08-21 11:38:35 -0500

    finished lesson on adding elements via the dom

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,5 +1,15 @@
 //DOM -Document Object Model
+const notes = [{
+    title: 'Vacation ideas',
+    body: 'I would like to visit Valencia'
+},{ 
+    title: 'Meditation',
+    body: 'I would like to meditate twice a day'
 
+}, {
+    title: 'Organize Desk',
+    body: 'I would like to organize my desk space'
+}]
 //This finds the first instance of what you're looking for, and removes it. This one looks for the paragraph tag.
 //Query and remove
 //const p = document.querySelector('p')
@@ -15,4 +25,8 @@ ps.forEach(function (p) {
     //p.remove() //removes the values
 })
 
+//Add a new element 
+const newParagraph = document.createElement('p')
+newParagraph.textContent = 'This is a new element from Javascript'
+document.querySelector('body').appendChild(newParagraph)
 
--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -1,10 +1,44 @@
+const todos = [{
+    text: 'get groceries',
+    completed: false
+},{
+    text: 'do laundry',
+    completed: true
+}, {
+    text: 'clean room',
+    completed: false
+}, {
+    text: 'plan vacation',
+    completed: true
+}, {
+    text: 'organize closet',
+    completed: false
+}]
 
 
-const paragraphs = document.querySelectorAll('p') 
 
-paragraphs.forEach(function (p) {
-    if (p.textContent.includes('the')) {
-        p.remove() //removes the values
-    }
+// const paragraphs = document.querySelectorAll('p') 
+
+// paragraphs.forEach(function (p) {
+//     if (p.textContent.includes('the')) {
+//         p.remove() //removes the values
+//     }
   
+// })
+
+//create a paragraph that says how many todos are left (value of false for completed)
+
+const incompleteTodos = todos.filter(function(todo) {
+    return !todo.completed
+})
+
+const summary = document.createElement('h2') 
+summary.textContent = `You have ${incompleteTodos.length} todos left`
+document.querySelector('body').appendChild(summary)
+
+
+todos.forEach(function (todo){
+    const p = document.createElement('p')
+    p.textContent = todo.text
+    document.querySelector('body').appendChild(p)
 })
